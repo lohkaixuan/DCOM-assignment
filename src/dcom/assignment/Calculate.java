2fec9c564ca83fa009f2c622608282f6e45c1101
lohkaixuan <lohkaixuan03@gmail.com>
2025-06-05 20:29:15 +0800

    feat: add calculate

---
+++ src/dcom/assignment/Calculate.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dcom.assignment;
+
+import java.util.Scanner;
+
+public class Calculate {
+    double netpay, netde, hours, basic, tax, grosspay, deduction;
+
+    public void sethours(double hours) {
+        this.hours = hours;
+    }
+
+    public void setbasic(double basic) {
+        this.basic = basic;
+    }
+
+    public void settax(double tax) {
+        this.tax = tax;
+    }
+
+    public double getnetpay() {
+        grosspay = hours * basic;
+        return grosspay;
+    }
+
+    public double getdeduction() {
+        deduction = grosspay * tax;
+        return deduction;
+    }
+
+    public double netpay() {
+        grosspay = getnetpay();
+        netpay = grosspay * (1 - tax);
+        return netpay;
+    }
+    /*
+     * public double netde(){
+     * grosspay = getnetpay();
+     * deduction = getdeduction();
+     * netde = grosspay - deduction;
+     * return netde;
+     * }
+     */
+}
